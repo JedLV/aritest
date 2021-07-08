cb0841f24c9e65ccb025e0459760aeccdcd1fbe7
编程 <1660873558@qq.com>
2021-07-08 09:12:04 +0800

    算数运算符二

---
+++ src/Aritest2.java
@@ -0,0 +1,23 @@
+public class Aritest2 {
+    public static void main (String[] args){
+
+        //%： 取余值运算
+        int m1 = 12;
+        int n1 = 5;
+        System.out.println("m1 % n1 = " + m1 % n1);
+
+        int m2 = -12;
+        int n2 = 5;
+        System.out.println("m2 % n1 = " + m2 % n2);
+
+        int m3 = 12;
+        int n3 = -5;
+        System.out.println("m3 % n3 = " + m3 % n3);
+
+        int m4 = -12;
+        int n4 = -5;
+        System.out.println("m4 % 4 = " + m4 % n4);
+
+        //结果的符号与被模数的符号相同
+    }
+}
