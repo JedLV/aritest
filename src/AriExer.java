ef7d88646d1cd2ee0ae54c532c4d7c89261f5573
编程 <1660873558@qq.com>
2021-07-08 10:12:56 +0800

    算数练习

---
+++ src/AriExer.java
@@ -0,0 +1,21 @@
+public class AriExer {
+    public static void main (String[] args){
+
+        int num = 187;
+
+        int bai =num / 100;
+        int shi = num % 100 / 10; //int shi = num / 10 % 10;
+        int ge = num % 10;
+
+        System.out.println("百位数为：" + bai);
+        System.out.println("十位数为：" + shi);
+        System.out.println("个位数为：" + ge);
+
+        //随意给出一个整数，打印显示它的个位数，十位数，百位数的值
+        //数字xxx的情况如下：
+        //个位数：
+        //十位数：
+        //百位数：
+
+    }
+}
