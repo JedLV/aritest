20a36b70beff504379f09db0f56668f687f5f1c4
编程 <1660873558@qq.com>
2021-07-08 12:55:02 +0800

    赋值运算符的使用

---
+++ src/SetValueTest.java
@@ -0,0 +1,22 @@
+public class SetValueTest {
+    public static void main (String[] args){
+        //赋值符号: =
+        int i1 = 10;
+        int j1 = 10;
+
+        int i2,j2;
+        //连续赋值
+        i2 = j2 =10;
+
+        int i3 =10, j3 =20;
+
+        //***************
+        int num1 = 10;
+        num1 += 2; //num1 = num1 + 2;
+        System.out.println(num1);
+
+        int num2 = 12;
+        num2 %= 5; //num2 = num2 % 5;
+        System.out.println(num2);
+    }
+}
